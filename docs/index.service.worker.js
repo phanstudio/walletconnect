a84180bf992a76581103749608880edeb1ce505b
phanstudio <donenoelle@gmail.com>
2025-03-17 17:04:25 +0100

    update

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742227033|66870458125';
+const CACHE_VERSION = '1742227420|67257263323';
 /** @type {string} */
 const CACHE_PREFIX = 'Block Wars-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
